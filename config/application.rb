04d4bbf00861cc44a936f49055cb90ead4d6ad50
Bernardo Galindo <bernardo466@gmail.com>
2012-08-20 12:00:08 -0500

    fix initialize assets precompile on heroku

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Micaffetos
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    
+    # Fix for assets on Heroku run assets precompile  http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
