740d6fac060e855e8506db9cf9614a0a0435eac3
Bernardo Galindo <bernardo466@gmail.com>
2012-08-19 21:01:18 -0500

    first commit about caffetos page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micaffetos::Application.config.secret_token = '8e2413aaf6f206393faea931af2ca5675ad272ad233eacc2e265429167debcf13ca63c019d78ce6572ae847ff69b72fc1e67275254a6936e443822ab709f3bc0'
